fe2b36343b3d3154ae3a826bb0d4c2b4ff9ef1f5
Fu-Ching Yang <fuching@marvell.com>
2017-01-06 14:17:10 +0800

    Add script to cross build for ARM platform

---
+++ cross_build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+make LDFLAGS=-static CC=aarch64-linux-gnu-gcc trace-cmd PATH=$PATH:/home/fuching/mrvl_soc/A7K/Software_Sep_2016/Toolchain/armv8/le/aarch64v8-marvell-linux-gnu-5.2.1_x86_64_20151110/bin
